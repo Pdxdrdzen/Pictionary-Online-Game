d309d8d618539e91d999a92d811c844759d58447
PDrdzen <pdrdzen@interia.pl>
2026-03-17 10:07:26 +0100

    Test TCP connections server/client, working message send

---
+++ src/client.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+import json
+
+HOST='127.0.0.1'
+PORT=12345
+
+def send_message(client_socket, message: dict):
+    data=json.dumps(message)+'\n'
+    client_socket.sendall(data.encode('utf-8'))
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        print(f"Connected to {HOST}:{PORT}")
+
+        while True:
+            msg=input("Write a message to send: ");
+            if msg.lower()=='quit':
+                break;
+            send_message(s,{"type":"message","payload":msg})
+            data=s.recv(1024).decode('utf-8')
+            print(f"Received from {HOST}:{PORT}: {data}")
+
+if __name__ == '__main__':
+    main()
---
+++ src/server.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+from http import client
+
+SERVER_HOST='127.0.0.1'
+SERVER_PORT=12345
+
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) #restart bugfix
+server.bind((SERVER_HOST,SERVER_PORT))
+server.listen()
+
+clients=[]
+print(f"TCP server listening on {SERVER_HOST}:{SERVER_PORT}")
+
+def handle_client(client_socket,client_addr):
+    print(f"Client {client_socket} connected")
+    clients.append(client_socket)
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            if not message:
+                break
+            print(f"From {client_addr}:{message}")
+            for client in clients:
+                try:
+                    client.send(f"echo: {message}".encode('utf-8'))
+                except:
+                    clients.remove(client)
+        except:
+            break
+    print(f"Client {client_addr} disconnected")
+    if client_socket in clients:
+        clients.remove(client_socket)
+    client_socket.close()
+try:
+    while True:
+        client_socket, client_addr = server.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,client_addr))
+        client_thread.daemon=True
+        client_thread.start()
+except KeyboardInterrupt:
+    print("\nClosing server...")
+finally:
+    for client in clients:
+        client.close()
+    server.close()
+    print("Server closed")
